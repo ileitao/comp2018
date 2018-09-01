42c8eb87efa3f459b720860e4a39c752352edfe5
ileitao <ignacio.leitao@gmail.com>
2018-08-31 21:41:47 -0300

    Add lecture char by char, reset current char pointer, for basic lecture and running example

--- Compilador/src/compilador/Compilador.java
+++ Compilador/src/compilador/Compilador.java
@@ -1,5 +1,7 @@
 package compilador;
 
+import java.io.IOException;
+
 /**
  * Clase Compilador
  * 
@@ -11,12 +13,25 @@ public class Compilador {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String rutaDelArchivo = "codigo.txt";
         if (args.length != 0) {
             rutaDelArchivo = (String)args[0];
         }
         
+        //// Probando que lector de archivos funciona correctamente
         LectorDeArchivo lector = new LectorDeArchivo(rutaDelArchivo);
+        System.out.println(lector.getLineaActual());
+        lector.leerLinea();
+        System.out.println(lector.getLineaActual());
+        
+        lector.resetearLector();
+        for (int i = 0; i < 50; i++) {
+            lector.leerChar();
+        }
+        
+        //// TablaDeSimbolos tablaDeSimbolos = new TablaDeSimbolos();
+        //// LectorDeArchivo lectorDeArchivo = new LectorDeArchivo(rutaDelAchivo);
+        //// AnalizadorLexico analizadorLexico = new AnalizadorLexico(lectorDeArchivo, tablaDeSimbolos);
     }
 }
--- Compilador/src/compilador/LectorDeArchivo.java
+++ Compilador/src/compilador/LectorDeArchivo.java
@@ -7,7 +7,6 @@ package compilador;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -29,9 +28,12 @@ public class LectorDeArchivo {
 
     private String lineaAnterior;
     
+    private String rutaDelArchivo;
+    
     public LectorDeArchivo(String archivo) {
         try {
-            this.in = new BufferedReader(new FileReader(new File(archivo)));
+            this.rutaDelArchivo = archivo;
+            this.in = new BufferedReader(new FileReader(new File(this.rutaDelArchivo)));
             System.out.println("Archivo abierto exitosamente.");
             this.puntero = 0;
             this.lineaActual = in.readLine();
@@ -42,6 +44,11 @@ public class LectorDeArchivo {
         }
     }
     
+    /**
+     * leerLinea obtiene una linea entera dada un archivo
+     * y la setea en lineaActual, guardando la linea anterior
+     * en lineaAnterior e incrementando el indice nroLinea
+     */
     public void leerLinea() {
         try {
             this.lineaAnterior = this.lineaActual;
@@ -54,6 +61,80 @@ public class LectorDeArchivo {
         }
     }
     
+    public String leerChar() throws IOException {
+        String character = "";
+        //// Tengo una linea para leer
+        if (this.lineaActual != null) {
+            //// El puntero superó el largo de la linea leída
+            if (this.puntero > this.lineaActual.length()) {
+                //// Leo una linea nueva
+                this.leerLinea();
+                //// Reinicio el puntero a la primer posición
+                this.puntero = 0;
+                //// Pido el primer caracter recursivamente
+                return this.leerChar();
+            } else if (this.puntero == this.lineaActual.length()) {
+                character = "/n";
+            } else {
+                char characterLeido = (this.lineaActual.charAt(puntero));
+                character = "" + characterLeido;
+                switch (characterLeido){
+                  //case '\t': result="/t"; break;
+                    case ' ': character = " "; break;
+                }  
+                System.out.println("Caracter Parcial: " + character + " en nro linea: " + this.nroLinea + " puntero en posicion " + this.puntero);
+            }
+
+            this.puntero++;
+        } else {
+            //// Fin del archivo = devolver caracter simbolico
+            //// para avanzar al ultimo estado (x ej "$")
+            character = "";
+        }
+
+        return character;    
+    }
+    
+    /**
+     * retrocederPuntero mueve puntero en una posición 
+     * se considera el caso en que puntero está en la 
+     * posición 0 y es necesario volver a la linea anterior
+     */
+    public void retrocederPuntero() {
+        if (this.puntero <= 0) {
+            this.puntero = 0;
+            if (this.nroLinea > 0) {
+                this.nroLinea--;    
+            }
+            
+            this.lineaActual = this.lineaAnterior;            
+        } else {
+            this.puntero--;
+        }
+    }
+    
+    /**
+     * Resetea el lector al inicio del archivo almacenado en rutaDelArchivo
+     * @throws IOException 
+     */
+    public void resetearLector() throws IOException {
+        try {
+            this.in.close();
+            this.in = new BufferedReader(new FileReader(this.rutaDelArchivo));
+            this.puntero = 0;
+            this.nroLinea = 0;
+            this.lineaActual = in.readLine();
+            this.lineaAnterior = "";   
+        }
+        catch (IOException exc) {
+            System.out.println("No se pudo resetear el archivo.");
+        }
+    }
+    
+    public BufferedReader getIn() {
+        return this.in;
+    }
+    
     public String getLineaActual() {
         return this.lineaActual;
     }
@@ -64,5 +145,13 @@ public class LectorDeArchivo {
     
     public int getNroLinea() {
         return this.nroLinea;
+    }
+    
+    public String getRutaDelArchivo() {
+        return this.rutaDelArchivo;
+    }
+    
+    public void setRutaDelArchivo(String rutaDelArchivo) {
+        this.rutaDelArchivo = rutaDelArchivo;
     }
 }
