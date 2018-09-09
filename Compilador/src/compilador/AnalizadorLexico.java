14594dc0321fffda041108ce8eabdf1a4fe7e315
ileitao <ignacio.leitao@gmail.com>
2018-09-09 10:30:39 -0300

    Adding getFila method used for getting matEstados line number based on a character, adding retrocederLectura method used for call LectorDeArchivo retrocederPuntero method, adding some logic inside getToken method

--- Compilador/src/compilador/AnalizadorLexico.java
+++ Compilador/src/compilador/AnalizadorLexico.java
@@ -1,5 +1,6 @@
 package compilador;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,98 +13,210 @@ import java.util.List;
  */
 public class AnalizadorLexico {
 
-	private static final int ESTADO_INICIAL = 0;
+    private static final int ESTADO_INICIAL = 0;
 
-	private static final int ESTADO_FINAL = -1;
+    private static final int ESTADO_FINAL = -1;
 
-	private int estadoActual = ESTADO_INICIAL;
+    private int estadoActual = ESTADO_INICIAL;
 
-	private int lineaActual = 1;
-        
-	// Representa la matriz de estados y simbolos.
-	// Almacena el indice del estado al que se avanza.
-        // Filas: clase del simbolo - Columnas: nro de estado (0-16)
-	private int[][] matEstados =
-        {
-            {10,  1, -1, -1, -1, -1, -1, -1, -1, -1, 10, 11, 12, -1, -1, -1, -1}, // [a-z] - [A-Z]
-            {10,  1, -1, -1, -1,  6, -1, -1,  6, -1, 10, 11, 12, -1, -1, -1, -1}, // "F"
-            {10,  1, -1,  4, -1, -1, -1, -1, -1, -1, 10, 11, 12, -1, -1, -1, -1}, // "u"
-            {10,  1, -1, -1, -1, -1, -1, -1, -1, -1, 10, 11, 12, -1, -1, -1, -1}, // "i"
-            { 2,  1,  2, -1, -1,  8,  9,  9,  8,  9, -1, 11, 12, -1, -1, -1, -1}, // [0-9]
-            { 5, -1,  5, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "."
-            { 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 10, 11, 12, -1, -1, -1, -1}, // "_"
-            {-1, -1, -1, -1, -1, -1,  7, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "+"
-            {-1, -1,  3, -1, -1, -1,  7, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "-"
-            {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "*"
-            {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "/"
-            {16, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // ":"
-            {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "="
-            {13, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "<"
-            {14, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // ">"
-            {15, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "!"
-            {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "("
-            {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // ")"
-            {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "{"
-            {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "}"
-            {11, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  0, 12, -1, -1, -1, -1}, // "#"
-            {12, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, -1, -1, -1, -1, -1}, // "`"
-            { 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // " "
-            { 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, -1, -1, -1, -1, -1}, // "/n"
-            { 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "/t"
-            { 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "otro"
-            {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, // $
-            
-        };
+    private static final int CANTIDAD_ESTADOS = 17;
 
-	// Representa la accion semantica asociada a cada estado.
-	private AccionSemantica[][] matAccSem;
+    private static final int CANTIDAD_SIMBOLOS = 27;
 
-	private TablaDeSimbolos tablaSimbolos;
+    private int lineaActual = 1;
 
-	// Tira de tokens
-	private List<Token> tokens;
+    private String lexemaParcial;
 
-	// Lector de caracteres
-	private LectorDeArchivo lector;
-	
-	private Logger logger;
+    private Character charActual;
 
-	public AnalizadorLexico(LectorDeArchivo lector) {
-            // this.matEstados = new int[cantEstados][cantSimbolos];
-            // this.matAccSem = new AccionSemantica[cantEstados][cantSimbolos];
-            this.lector = lector;
-            this.tokens = new ArrayList<>();
-            logger = new Logger();
-	}
+    // Representa la matriz de estados y simbolos.
+    // Almacena el indice del estado al que se avanza.
+    // Filas: clase del simbolo - Columnas: nro de estado (0-16)
+    private int[][] matEstados =
+    {
+        {10,  1, -1, -1, -1, -1, -1, -1, -1, -1, 10, 11, 12, -1, -1, -1, -1}, // [a-z] - [A-Z]
+        {10,  1, -1, -1, -1,  6, -1, -1,  6, -1, 10, 11, 12, -1, -1, -1, -1}, // "F"
+        {10,  1, -1,  4, -1, -1, -1, -1, -1, -1, 10, 11, 12, -1, -1, -1, -1}, // "u"
+        {10,  1, -1, -1, -1, -1, -1, -1, -1, -1, 10, 11, 12, -1, -1, -1, -1}, // "i"
+        { 2,  1,  2, -1, -1,  8,  9,  9,  8,  9, -1, 11, 12, -1, -1, -1, -1}, // [0-9]
+        { 5, -1,  5, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "."
+        { 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 10, 11, 12, -1, -1, -1, -1}, // "_"
+        {-1, -1, -1, -1, -1, -1,  7, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "+"
+        {-1, -1,  3, -1, -1, -1,  7, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "-"
+        {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "*"
+        {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "/"
+        {16, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // ":"
+        {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "="
+        {13, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "<"
+        {14, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // ">"
+        {15, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "!"
+        {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "("
+        {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // ")"
+        {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "{"
+        {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "}"
+        {11, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  0, 12, -1, -1, -1, -1}, // "#"
+        {12, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, -1, -1, -1, -1, -1}, // "`"
+        { 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // " "
+        { 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, -1, -1, -1, -1, -1}, // "/n"
+        { 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "/t"
+        { 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1}, // "otro"
+        {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, // $
 
-	public void setEstadoActual(int estadoActual) {
-            this.estadoActual = estadoActual;
-	}
+    };
 
-	public int getEstadoActual() {
-            return this.estadoActual;
-	}
+    // Acciones semánticas
+    //private AccionSemantica AS_1 = new AccionSemantica01HacerNada(this);
+    //private AccionSemantica AS_2 = new AccionSemantica02ValidarEnteroSinSigno(this);
+    //private AccionSemantica AS_3 = new AccionSemantica03ValidarFlotante(this);
+    //private AccionSemantica AS_4 = new AccionSemantica04ValidarIdentificador(this);
+    //private AccionSemantica AS_5 = new AccionSemantica05ValidarPalabraReservada(this);
+    //private AccionSemantica AS_6 = new ...;
+    //private AccionSemantica AS_7 = new ...;
+    //private AccionSemantica AS_8 = new ...;
 
-	public void setLineaActual(int lineaActual) {
-            this.lineaActual = lineaActual;
-	}
+    // Representa la accion semantica asociada a cada estado.
+    private AccionSemantica[][] matAccSem;
 
-	public int getLineaActual() {
-            return this.lineaActual;
-	}
+    private TablaDeSimbolos tablaSimbolos;
 
-	public TablaDeSimbolos getTablaSimbolos() {
-            return tablaSimbolos;
-	}
+    // Tira de tokens
+    private List<Token> tokens;
 
-	public List<Token> getTokens() {
-            return tokens;
-	}
-	
-	public Token getToken() {
-            //TODO realizar la logica de avance de estados hasta obtener un token.
-            //Se debe invocar al lector de archivos mientras los estados de la matriz no lleguen al estado final
-            //En caso de error, se debera loguear.
-            return null;
-	}
+    // Lector de caracteres
+    private LectorDeArchivo lector;
+
+    private Logger logger;
+
+    public AnalizadorLexico(LectorDeArchivo lector) {
+        this.matEstados = new int[AnalizadorLexico.CANTIDAD_ESTADOS][AnalizadorLexico.CANTIDAD_SIMBOLOS];
+        //this.matAccSem = new AccionSemantica[[AnalizadorLexico.CANTIDAD_ESTADOS][AnalizadorLexico.CANTIDAD_SIMBOLOS];
+        this.lector = lector;
+        this.tokens = new ArrayList<>();
+        logger = new Logger();
+    }
+
+    public void setEstadoActual(int estadoActual) {
+        this.estadoActual = estadoActual;
+    }
+
+    public int getEstadoActual() {
+        return this.estadoActual;
+    }
+
+    public void setLineaActual(int lineaActual) {
+        this.lineaActual = lineaActual;
+    }
+
+    public int getLineaActual() {
+        return this.lineaActual;
+    }
+
+    public TablaDeSimbolos getTablaSimbolos() {
+        return tablaSimbolos;
+    }
+
+    public List<Token> getTokens() {
+        return tokens;
+    }
+    
+    public String getLexemaParcial() {
+        return this.lexemaParcial;
+    }
+
+    public void setLexemaParcial(String nuevoLexema) {
+        this.lexemaParcial = nuevoLexema;
+    }
+    
+    public Character getCharActual() {
+        return this.charActual;
+    }
+    
+    public void setCharActual(Character caracter) {
+        this.charActual = caracter;
+    }
+    
+    public Token getToken() throws IOException {
+        //TODO realizar la logica de avance de estados hasta obtener un token.
+        //Se debe invocar al lector de archivos mientras los estados de la matriz no lleguen al estado final
+        //En caso de error, se debera loguear.
+        this.lexemaParcial = new String();
+        this.estadoActual = AnalizadorLexico.ESTADO_INICIAL;
+        //Se itera hasta llegar al estado final
+        while (this.estadoActual != AnalizadorLexico.ESTADO_FINAL) {
+            //Leer un char
+            this.charActual = this.lector.leerChar();
+            //Ir armando el lexema parcial
+            this.lexemaParcial += this.charActual;
+        }
+
+        return null;
+    }
+    
+    public void retrocederLectura() {
+        this.lector.retrocederPuntero();
+    }
+
+    /**
+     * Retorna el número de fila a partir del caracter recibido
+     * 
+     * @param simboloActual
+     * @return
+     */
+    private int getFila(Character caracter) {
+        if ((((caracter >= 'a') && (caracter <= 'z')) || ((caracter >= 'A') && (caracter <= 'Z'))) && ((caracter != 'F') && (caracter != 'u') && (caracter != 'i'))) {
+            return 0;
+        } else if (caracter == 'F') {
+            return 1;
+        } else if (caracter == 'u') {
+            return 2;
+        } else if (caracter == 'i') {
+            return 3;
+        } else if ((caracter >= '0') && (caracter <= '9')) {
+            return 4;
+        } else if(caracter == '.'){
+            return 5;
+        } else if (caracter == '_') {
+            return 6;
+        } else if (caracter == '+') {
+            return 7;
+        } else if(caracter == '-') {
+            return 8;
+        } else if (caracter == '*') {
+            return 9;
+        } else if (caracter=='/') {
+            return 10;
+        } else if (caracter == ':') {
+            return 11;
+        } else if  (caracter == '=') {
+            return 12;
+        } else if (caracter == '<') {
+            return 13;
+        } else if (caracter == '>') {
+            return 14;
+        } else if (caracter == '!') {
+            return 15;
+        } else if (caracter == '(') {
+            return 16;
+        } else if (caracter == ')') {
+            return 17;
+        } else if (caracter == '{') {
+            return 18;
+        } else if (caracter == '}') {
+            return 19;
+        } else if (caracter == '#') {
+            return 20;
+        } else if (caracter == ' ') {
+            return 21;
+        } else if (caracter == '\'') {
+            return 22;
+        } else if (caracter == '\n') {
+            return 23;
+        } else if (caracter == '\t') {
+            return 24;
+        } else if (caracter == '$') {
+            return 26;
+        } else {
+            return 25;
+        }
+    }   
 }
