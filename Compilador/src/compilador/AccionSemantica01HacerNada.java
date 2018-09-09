09a1fc2bd22de1faa3bb22c6f9a306dab17a3139
ileitao <ignacio.leitao@gmail.com>
2018-09-09 10:32:53 -0300

    Adding new semantic actions, AccionSemantica03ValidarFlotante includes some logic

--- Compilador/src/compilador/AccionSemantica01HacerNada.java
+++ Compilador/src/compilador/AccionSemantica01HacerNada.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package compilador;
 
 /**
@@ -19,8 +14,6 @@ public class AccionSemantica01HacerNada extends AccionSemantica {
     }
 
     @Override
-    public void execute() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    public void execute() {}
     
 }
---
+++ Compilador/src/compilador/AccionSemantica05ValidarPalabraReservada.java
@@ -0,0 +1,21 @@
+package compilador;
+
+/**
+ * Clase AccionSemantica05ValidarPalabraReservada
+ * 
+ * @author ileitao
+ * @author gmaiola
+ * @author ltapia
+ */
+public class AccionSemantica05ValidarPalabraReservada extends AccionSemantica {
+
+    public AccionSemantica05ValidarPalabraReservada(AnalizadorLexico analizadorLexico) {
+        super(analizadorLexico);
+    }
+
+    @Override
+    public void execute() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
